9c03b7ea7ab21683ca5d30ae9da66b1d44afc8dc
hasekawayoko <52697095+hasekawayoko@users.noreply.github.com>
2026-03-03 17:12:13 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/nihongo-go/', // 👈 这一行最关键，必须和你的 GitHub 仓库名一致
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
